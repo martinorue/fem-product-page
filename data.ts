bedbf1f0844e4280def222e66c8e3bdd48ba57dc
martinorue <martin.orue008@gmail.com>
2023-08-31 15:26:36 -0300

    test images on deploy

--- data.ts
+++ data.ts
@@ -1,4 +1,5 @@
 import { Product } from "./types"
+
 export const products: Product[] = [
     {
         id: 1,
@@ -7,16 +8,16 @@ export const products: Product[] = [
         price: 125,
         discountPercentage: 50,
         images: [
-            { id: 1, src: '../assets/images/image-product-1.jpg' },
-            { id: 2, src: 'src/assets/images/image-product-2.jpg' },
-            { id: 3, src: 'src/assets/images/image-product-3.jpg' },
-            { id: 4, src: 'src/assets/images/image-product-4.jpg' }
+            { id: 1, src: '../src/assets/images/image-product-1.jpg' },
+            { id: 2, src: '../src/assets/images/image-product-2.jpg' },
+            { id: 3, src: '../src/assets/images/image-product-3.jpg' },
+            { id: 4, src: '../src/assets/images/image-product-4.jpg' }
         ],
         thumbnails: [
-            { id: 5, src: 'src/assets/images/image-product-1-thumbnail.jpg' },
-            { id: 6, src: 'src/assets/images/image-product-2-thumbnail.jpg' },
-            { id: 7, src: 'src/assets/images/image-product-3-thumbnail.jpg' },
-            { id: 8, src: 'src/assets/images/image-product-4-thumbnail.jpg' }
+            { id: 5, src: '../src/assets/images/image-product-1-thumbnail.jpg' },
+            { id: 6, src: '../src/assets/images/image-product-2-thumbnail.jpg' },
+            { id: 7, src: '../src/assets/images/image-product-3-thumbnail.jpg' },
+            { id: 8, src: '../src/assets/images/image-product-4-thumbnail.jpg' }
         ]
     }
 ]
